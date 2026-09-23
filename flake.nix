96768736417f7d4a96a3c362dbcff6306c3844c1
David Mödinger <david.moedinger@ketzu.net>
2026-09-23 16:21:59 +0200

    Add unstable Nixpkgs input and update home config

--- flake.nix
+++ flake.nix
@@ -1,17 +1,22 @@
 {
   description = "home";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations."david" = home-manager.lib.homeManagerConfiguration {
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }: {
+    homeConfigurations."claude" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = {
+        pkgs-unstable = import nixpkgs-unstable {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
       modules = [ ./home.nix ];
     };
   };
